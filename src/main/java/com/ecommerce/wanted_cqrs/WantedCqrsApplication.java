c4c0d8eaab73dee3b3d766a8484bf1030e8c7f52
leejaehun <ljh0901@mlsoft.com>
2025-05-12 16:53:48 +0900

    feat : project init

---
+++ src/main/java/com/ecommerce/wanted_cqrs/WantedCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.wanted_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedCqrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/wanted_cqrs/WantedCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.wanted_cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedCqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
